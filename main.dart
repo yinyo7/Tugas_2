4430713e8bc9835cd702efb5a9ec574978d663cd
Gideon N. M <56784171+yinyo7@users.noreply.github.com>
2021-03-28 02:29:13 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tugas2_1/tugas2_1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Tugas());
+
+  }
+}
+
+
+
